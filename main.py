5eb72817165ed866ff26533691d978aa2a2f3950
AgnusAlfa <umanomedials@gmail.com>
2026-03-18 19:22:19 +0000

    v1

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+print("hola gente linda")
+
+# vARIABLES
+
+nombre = "jo"
+edad = 35
+altura = 2.12
+es_docente = False
+ciudad = "Vancouver"
+
+# TIPOS DE DATOS
+
+str #esto es un texto
+int #esto es un entero
+float #esto es un flotante
+bool #esto es verdadero o falso
+
+#INPUT
+
+texto = input()
+print(texto)
