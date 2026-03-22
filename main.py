5ffb40272bc5ab97d05754b39d4eabdfed85f337
AgnusAlfa <umanomedials@gmail.com>
2026-03-22 15:10:43 +0000

    math prueba

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-
 print("hola gente linda")
 
 # vARIABLES
@@ -17,7 +16,45 @@ int #esto es un entero
 float #esto es un flotante
 bool #esto es verdadero o falso
 
-#INPUT
 
-texto = input()
-print(texto)
+#INPUT
+precio = input("escribe un numero : ") #pedir por consola un tipo de dato stren (texto)
+int(precio) #convertir a entero el str
+precio =+ 10 #el precio sumado + 10 ya que ahora es numero entero
+print(precio) 
+
+## CONDICIONALES
+
+
+#if condicion:
+#    codigo 
+
+edad = 20 
+
+if edad >= 18:
+    print("puede entrar a la disco")
+
+contador = 0
+
+
+# WHILE
+
+while contador < 10: #bucle si es true o false
+    print(contador)
+    contador += 1
+
+while True:
+    texto = input("escribe para salir de bucle")
+
+    if texto == "salir":
+        break
+
+#FOR
+
+#for elemento in lista
+#    codigo
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in lista:
+    print(i)
---
+++ mat.py
@@ -0,0 +1,19 @@
+import math
+
+
+
+
+print(math.pi)
+
+
+
+
+numero = 2567
+
+raiz = math.sqrt(numero)
+print(raiz)
+
+numero1 = 1
+numero2 = 2
+resultado = numero + numero1
+print(resultado)
